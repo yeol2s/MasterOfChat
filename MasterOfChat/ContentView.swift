c7020fafb01d4607be1cd677d2438c3c1efe8c2f
syyoo <yeol2s@naver.com>
2024-05-02 03:42:03 +0900

    FIRST COMMIT

---
+++ MasterOfChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MasterOfChat
+//
+//  Created by 유성열 on 5/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MasterOfChat/MasterOfChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterOfChatApp.swift
+//  MasterOfChat
+//
+//  Created by 유성열 on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MasterOfChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
